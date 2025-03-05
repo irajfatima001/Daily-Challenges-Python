0791623164b98f36e453c890abee9798540caae1
irajfatima001 <irajf935@gmail.com>
2025-03-06 01:43:17 +0500

    day9

---
+++ day-9/app.py
@@ -0,0 +1,18 @@
+
+import re
+print("------Password Strength Checker------")
+
+password = input("Enter your password:")
+
+pass_length=len(password)
+
+if pass_length < 6 :
+    print("your password is weak! Try atleast 6 character.")
+elif (len(password) >= 8 and
+          re.search(r'[A-Z]', password) and 
+          re.search(r'[a-z]', password) and 
+          re.search(r'\d', password) and    
+          re.search(r'[!@#$%^&*(),.?":{}|<>]', password)):  
+        print (" Strong Password! Great job! ")
+else:
+    print(" Your password is MODERATE! Try adding uppercase, numbers, and special characters.")    
